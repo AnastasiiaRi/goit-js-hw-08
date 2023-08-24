12b0f1ab6affa4868d57233c4ff78a9726df2f77
AnastasiiaRi <archi.bocharova@gmail.com>
2023-08-24 10:03:00 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const image = galleryItems
+.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original.value}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+})
+.join("");
+gallery.insertAdjacentHTML("afterbegin", image);
+
+gallery.addEventListener("click", e => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onKeydownEsc);
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', onKeydownEsc);
+      },
+    },
+  );
+  const onKeydownEsc = e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  };
+  instance.show();
+});
