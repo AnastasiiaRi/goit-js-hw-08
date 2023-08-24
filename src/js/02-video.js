a07c6a7f225552fbd0033627edba167e6e474ab3
AnastasiiaRi <archi.bocharova@gmail.com>
2023-08-24 13:10:34 +0300

    т

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,12 @@
 import throttle from 'lodash.throttle'; 
-import Player from "@vimeo/player";   
+import Player from '@vimeo/player';   
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 const onPlay = function(seconds) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
+console.log(onPlay)
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
