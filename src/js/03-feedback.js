4ded0285fd176b55a04cc7c097bd4bdf2e09a03f
AnastasiiaRi <archi.bocharova@gmail.com>
2023-08-24 18:48:07 +0300

    h

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,19 @@
 import throttle from 'lodash.throttle'; 
 const form = document.querySelector(".login-form");
-form.addEventListener('input', throttle(onFormSubmit, 500));
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+    const formData = {
+        email,
+        message,
+    };
+    localStorage.setItem('feedback-form-state', formData);
+}
+
 function onFormSubmit(event) {
     event.preventDefault();
     const formElements = event.currentTarget.elements;
@@ -16,4 +28,4 @@ function onFormSubmit(event) {
     };
     console.log(formData);
     event.currentTarget.reset();
-    }
+}
